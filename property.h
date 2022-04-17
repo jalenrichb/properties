6fd8bbfc5410d7bd0d559fc6392dfd6bcf654674
Jalen Richburg <jalenrichburg@Jalens-MacBook-Air-2.local>
2022-04-17 12:43:55 -0400

    added Area, Value, and showProperty functions

--- property.h
+++ property.h
@@ -5,12 +5,14 @@ private:
 	int length;
 	int width;
 	string name;
+	float VpSF;
 
 public:
-	PROPERTY()
+	PROPERTY(){
 		length = 0;
 		width = 0;
 		name = "n/a";
+	}
 
 	PROPERTY(string n, int l, int w){
 	name = n;
@@ -18,5 +20,18 @@ public:
 	width = w;
 	
 	}
-};
 
+	int Area(){
+		return length * width;
+	}
+
+	float Value(){
+		return Area() * VpSF;
+	}
+
+	void showProperty(){
+		cout << "Name - " << name << endl;
+		cout << "Area - " << Area() << endl;
+		cout << "Value - " << Value() << endl;
+	}
+};
